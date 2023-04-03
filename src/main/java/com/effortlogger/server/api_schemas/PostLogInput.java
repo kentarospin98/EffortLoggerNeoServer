b2e17c25480606c5617516eace7367916c586ca6
Jeise1t <jeiselt@asu.edu>
2023-04-02 22:06:52 -0700

    Added PostLogInput and Output

---
+++ src/main/java/com/effortlogger/server/api_schemas/PostLogInput.java
@@ -0,0 +1,7 @@
+package com.effortlogger.server.api_schemas;
+
+import com.effortlogger.server.models.Log;
+
+public class PostLogInput {
+	public String username;
+}
---
+++ src/main/java/com/effortlogger/server/api_schemas/PostLogOutput.java
@@ -0,0 +1,35 @@
+package com.effortlogger.server.api_schemas;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import com.effortlogger.server.models.Log;
+
+public class PostLogOutput {
+    public String status;
+    public long id;
+    public String username;
+    public LocalDateTime startTime;
+    public LocalDateTime endTime;
+    public double deltaTime;
+    public String lifeCycleStep;
+    public String effortCategory;
+    public String deliverable;
+    
+
+    public PostLogOutput(String status) {
+        this.status = status;
+    }
+
+    public PostLogOutput(String status, Log log) {
+        this.status = status;
+        this.id = log.id;
+        this.username = log.username;
+        this.startTime = log.startTime;
+        this.endTime = log.endTime;
+        this.deltaTime = log.deltaTime;
+        this.lifeCycleStep = log.lifeCycleStep;
+        this.effortCategory = log.effortCategory;
+        this.deliverable = log.deliverable;
+    }
+}
